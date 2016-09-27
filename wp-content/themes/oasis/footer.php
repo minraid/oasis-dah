5d42404ac4f8ab864c8edc8e0e7296034e818b36
Олег Запотічний < >
2016-09-28 00:07:56 +0300

    mobile scroll

--- wp-content/themes/oasis/footer.php
+++ wp-content/themes/oasis/footer.php
@@ -35,12 +35,20 @@
 <script src="<?php bloginfo( 'template_url' ); ?>/js/comments.controller.js"></script>
 <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAaE5gtN_UQMu8hCgXZyUMuBemyNj2cQDk&callback=initMap"></script>
 <script>
-	document.addEventListener("DOMContentLoaded", function(){
-		if(document.body.clientWidth < 768) {
-			var y = document.querySelector('.content').offsetTop - 15;
-			window.scrollTo(0, y);
+	if(document.body.clientWidth < 768) {
+		var y = document.querySelector('.content').offsetTop - 15;
+		var step = y/100;
+		scrollTo(y, step);
+	}
+	function scrollTo(y, step) {
+		if(y<=0) {
+			return;
 		}
-	})
+		setTimeout(function(){
+			window.scrollTo(0, step);
+			scrollTo(y - step, step);
+		}, 10);
+	}
 </script>
 </body>
 </html>
